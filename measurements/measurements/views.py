1c19fdaec63c1f492642c43c0d1926255459f757
Federico Melo Barrero <f.melo@uniandes.edu.co>
2023-05-20 14:35:56 -0500

    Toma tu bendición

--- measurements/measurements/views.py
+++ measurements/measurements/views.py
@@ -16,9 +16,17 @@ def check_variable(data):
             return True
     return False
 
+def check_place(data):
+    r = requests.get(settings.PATH_PLACES, headers={"Accept":"application/json"})
+    places = r.json()
+    for place in places:
+        if data["place"] == place["id"]:
+            return True
+    return False
+
 def MeasurementList(request):
     queryset = Measurement.objects.all()
-    context = list(queryset.values('id', 'variable', 'value', 'unit', 'place', 'dateTime'))
+    context = list(queryset.values('id', 'place', 'value', 'unit', 'place', 'dateTime'))
     return JsonResponse(context, safe=False)
 
 def MeasurementCreate(request):
@@ -42,15 +50,19 @@ def MeasurementsCreate(request):
         data_json = json.loads(data)
         measurement_list = []
         for measurement in data_json:
-                    if check_variable(measurement) == True:
-                        db_measurement = Measurement()
-                        db_measurement.variable = measurement['variable']
-                        db_measurement.value = measurement['value']
-                        db_measurement.unit = measurement['unit']
-                        db_measurement.place = measurement['place']
-                        measurement_list.append(db_measurement)
-                    else:
-                        return HttpResponse("unsuccessfully created measurement. Variable does not exist")
+            # Se valida que el lugar exista!
+            place_exists = check_place(measurement)        
+            if check_variable(measurement) == True and place_exists:
+                db_measurement = Measurement()
+                db_measurement.variable = measurement['variable']
+                db_measurement.value = measurement['value']
+                db_measurement.unit = measurement['unit']
+                db_measurement.place = measurement['place']
+                measurement_list.append(db_measurement)
+            else:
+                return HttpResponse("unsuccessfully created measurement. Variable or Place does not exist")
         
         Measurement.objects.bulk_create(measurement_list)
         return HttpResponse("successfully created measurements")
+    
+    
